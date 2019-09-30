855c861c8d142e1add556b9c7a475dcd40e84bc2
shuotwang <shuotongwang@gmail.com>
2019-09-30 00:02:26 -0700

    Auto save function disabled

---
+++ RunningiPhone/New Trial/Cells/TimeInputTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TimeInputTableViewCell.swift
+//  
+//
+//  Created by Vincent on 2019/9/29.
+//
+
+import UIKit
+
+class TimeInputTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
